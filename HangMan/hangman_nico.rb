3b7215f6b88e96e1bb99ae3e580ac237cdb113a8
Nicolas Autzen <drbizeps@drbizeps-mba.local>
2012-01-26 14:36:22 +0100

    Meine Hangman Version

---
+++ HangMan/hangman_nico.rb
@@ -0,0 +1,52 @@
+words = %w[universitaet awe bremen ruby rails mvc dry bachelor master informatik iphone android tablet nicolas christian]
+
+puts "Let's play hangman."
+puts ""
+
+word = words[rand(words.count)] #holt sich ein zufaelliges Wort aus dem words-Array
+
+hangman_word = "" 
+word.length.times do 
+  hangman_word << '_'
+end
+
+puts "We got a word for you: \n" + hangman_word
+
+trials_left = 5
+false_trials = 0
+used_chars = "" #speichert die bereits eingegebenen Zeichen
+
+while trials_left > 0
+  
+  
+  puts "Enter a character:"
+  user_char = gets
+  user_char = user_char[0]
+  
+  if used_chars.include? user_char
+    puts "You allready used this one -> " + used_chars.chars.to_a.to_s
+    next
+  end
+  
+  used_chars << user_char
+  
+  if word.include? user_char
+    word.length.times do |i| 
+      hangman_word[i-1] = user_char if word[i-1] == user_char
+    end
+    puts "Correct"
+    puts hangman_word
+  elsif trials_left-1 == 0
+    puts "Wrong, you loose"
+    exit
+  else
+      trials_left = trials_left -1
+      puts "Wrong character, you got " + trials_left.to_s + " left. Please try again"
+  end
+  
+  if hangman_word == word
+    puts hangman_word + "\n You win"
+    exit
+  end
+end
+
