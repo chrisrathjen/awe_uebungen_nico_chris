c8ea157ba66450809a866d33da30b4c839e8efc2
Christian Rathjen <chrisr@tzi.de>
2012-01-26 17:55:52 +0100

    added word dictionary

--- HangMan/hangman_chris.rb
+++ HangMan/hangman_chris.rb
@@ -1,49 +1,54 @@
-#!/usr/bin/env ruby
+#vlt noch teilwörter als eingabe akzeptieren
 
 class HangMan
 	attr_reader :currentWord
-	attr_accessor :currentGuesses
+	attr_accessor :currentMistakes
 	attr_accessor :currentDisplay 
 	attr_accessor :alreadyTried
 	
 	def initialize()
-	  @currentWord = "testword"
-	  @currentGuesses = 0
-	  @currentDisplay = "-" * currentWord.length
-	  @alreadyTried = "You already tried: "
+	  words = %w[universitaet awe bremen ruby rails mvc dry bachelor master informatik iphone android tablet nicolas christian]
+	  @currentWord = words[rand(words.count)] # random Word
+	  @currentMistakes = 0 # number of mistakes
+	  @currentDisplay = "-" * currentWord.length # scrabled Word
+	  @alreadyTried = "You already tried: " # wrong guessed chars
   end
 end
 
 hm = HangMan.new
-puts "Welcome to HangMan"
-while hm.currentWord != hm.currentDisplay  
-  #STDOUT.sync = true
-  STDIN.sync = true
-  STDOUT.flush
-  puts "Please enter a Character"
+puts "Welcome to HangMan!\nYour word looks like this: #{hm.currentDisplay}"
+while hm.currentWord != hm.currentDisplay
+  puts "\nPlease enter a Character"
   input = gets.chomp
+  #filters inputs with more then 1 char
   if input.length != 1
     input = input[0]
+  end
+  
+  #filters empty inputs
+  if !input
+    input = " "
+    puts "You need to enter a Character"
   end
   
   if hm.currentWord.include? input
     if hm.currentDisplay.include? input
       puts "The Word already shows that letter"
-      hm.currentGuesses += 1
+      hm.currentMistakes += 1
     else
       for i in 0...hm.currentWord.length
         if hm.currentWord[i] == input
           hm.currentDisplay[i] = input
         end
       end
-      puts "Correct!\n#{hm.currentDisplay}"
+      puts "\nCorrect!\n\n#{hm.currentDisplay}"
     end
   else
     puts "Letter does not fit"
     hm.alreadyTried = hm.alreadyTried + " " + input
     puts hm.alreadyTried
     puts hm.currentDisplay
-    hm.currentGuesses += 1
+    hm.currentMistakes += 1
   end  
 end
-puts "You made #{hm.currentGuesses} mistakes" 
+puts "You made #{hm.currentMistakes} mistakes" 
