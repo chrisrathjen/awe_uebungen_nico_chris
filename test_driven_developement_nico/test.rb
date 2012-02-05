e74130e735d7a9e2f504f1795bca0e80ae148377
Nicolas Autzen <drbizeps@drbizeps-mba.local>
2012-02-05 16:17:06 +0100

    another test added

--- test_driven_developement_nico/test.rb
+++ test_driven_developement_nico/test.rb
@@ -1,5 +1,6 @@
 class RomanNumberGenerator
   def convert_number_to_roman(num)
+    return false if !(num.is_a? Integer)
     return false if num <= 0
     roman_number = ""
   end
@@ -8,7 +9,10 @@ end
 require 'test/unit'
 
 class MyTest < Test::Unit::TestCase
-  def test_should_fail_if_number_is_less_or_equal_zero
-    assert(!RomanNumberGenerator.new().convert_number_to_roman(-1))
+  def test_numbers_less_or_equal_zero
+    assert(!RomanNumberGenerator.new().convert_number_to_roman(-1), 'Argument can not be 0 or negative')
+  end
+  def test_typecheck
+      assert(!RomanNumberGenerator.new().convert_number_to_roman('a'), 'Argument is not a number')
   end
 end
