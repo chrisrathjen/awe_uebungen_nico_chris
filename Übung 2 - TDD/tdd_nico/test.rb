5cbbb253775a5295eb1b4d181d521feae7c96664
Nicolas Autzen <drbizeps@eduroam-0235.wlan.uni-bremen.de>
2012-02-07 12:59:37 +0100

    code ein bisschen aufgeraumt

--- Übung 2 - TDD/tdd_nico/test.rb
+++ Übung 2 - TDD/tdd_nico/test.rb
@@ -26,27 +26,27 @@ require 'test/unit'
 class MyTest < Test::Unit::TestCase
   
   def test_number_range
-    assert(!RomanNumberGenerator.new().convert_number_to_roman(0), 'Argument can not be 0')
-    assert(!RomanNumberGenerator.new().convert_number_to_roman(-1), 'Argument can not be negative')
-    assert(!RomanNumberGenerator.new().convert_number_to_roman(4000), 'Argument can not be greater then 3999')
+    assert(!RomanNumberGenerator.new.convert_number_to_roman(0), 'Argument can not be 0')
+    assert(!RomanNumberGenerator.new.convert_number_to_roman(-1), 'Argument can not be negative')
+    assert(!RomanNumberGenerator.new.convert_number_to_roman(4000), 'Argument can not be greater then 3999')
   end
   
   def test_typecheck
-    assert(!RomanNumberGenerator.new().convert_number_to_roman('a'), 'Argument is not a number')
-    assert(!RomanNumberGenerator.new().convert_number_to_roman(3.45), 'Argument is not an integer')
+    assert(!RomanNumberGenerator.new.convert_number_to_roman('a'), 'Argument is not a number')
+    assert(!RomanNumberGenerator.new.convert_number_to_roman(3.45), 'Argument is not an integer')
   end
   
   def test_check_output
-    assert('II' == RomanNumberGenerator.new().convert_number_to_roman(2), 'wrong output')
-    assert('V' == RomanNumberGenerator.new().convert_number_to_roman(5), 'wrong output')
-    assert('IX' == RomanNumberGenerator.new().convert_number_to_roman(9), 'wrong output')
-    assert('MMCXXII' == RomanNumberGenerator.new().convert_number_to_roman(2122), 'wrong output')
+    assert('II' == RomanNumberGenerator.new.convert_number_to_roman(2), 'wrong output')
+    assert('V' == RomanNumberGenerator.new.convert_number_to_roman(5), 'wrong output')
+    assert('IX' == RomanNumberGenerator.new.convert_number_to_roman(9), 'wrong output')
+    assert('MMCXXII' == RomanNumberGenerator.new.convert_number_to_roman(2122), 'wrong output')
   end
   
   def test_check_output_syntax
-    assert(!('IIII' == RomanNumberGenerator.new().convert_number_to_roman(4)), 'wrong output syntax')
-    assert(!('VIIII' == RomanNumberGenerator.new().convert_number_to_roman(9)), 'wrong output syntax')
-    assert(!('VV' == RomanNumberGenerator.new().convert_number_to_roman(10)), 'wrong output syntax')
+    assert(!('IIII' == RomanNumberGenerator.new.convert_number_to_roman(4)), 'wrong output syntax')
+    assert(!('VIIII' == RomanNumberGenerator.new.convert_number_to_roman(9)), 'wrong output syntax')
+    assert(!('VV' == RomanNumberGenerator.new.convert_number_to_roman(10)), 'wrong output syntax')
   end
   
 end
