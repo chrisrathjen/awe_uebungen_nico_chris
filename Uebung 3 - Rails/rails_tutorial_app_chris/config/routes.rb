7735a1c8a1f8403778f2b52dd124385f5075bb69
Christian Rathjen <chrisr@tzi.de>
2012-02-11 21:13:16 +0100

    costume layout and partials added

--- Uebung 3 - Rails/rails_tutorial_app_chris/config/routes.rb
+++ Uebung 3 - Rails/rails_tutorial_app_chris/config/routes.rb
@@ -1,6 +1,6 @@
 RailsTutorialAppChris::Application.routes.draw do
 
-  root to: 'static_pages#root'
+  root to: 'static_pages#home'
   match 'about', to: 'static_pages#about'
   match 'contact', to: 'static_pages#contact'
   match 'help', to: 'static_pages#help'
