e344aaff64161fcca9c1b713267dbe5f1cf4e912
Christian Rathjen <chrisr@tzi.de>
2012-02-11 21:55:35 +0100

    finish custom css and add signup page

--- Uebung 3 - Rails/rails_tutorial_app_chris/config/routes.rb
+++ Uebung 3 - Rails/rails_tutorial_app_chris/config/routes.rb
@@ -1,5 +1,9 @@
 RailsTutorialAppChris::Application.routes.draw do
 
+  get "users/new"
+
+  match '/signup', to: 'users#new'
+
   root to: 'static_pages#home'
   match 'about', to: 'static_pages#about'
   match 'contact', to: 'static_pages#contact'
