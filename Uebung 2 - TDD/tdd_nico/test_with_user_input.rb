41b9d7b6fed4f10bde43f6499080f3a8bf0d9530
Christian Rathjen <chrisr@tzi.de>
2012-02-11 22:06:38 +0100

    console frindly folder names

--- Uebung 2 - TDD/tdd_nico/test_with_user_input.rb
+++
@@ -1,6 +0,0 @@
-#zum manuellen Testen diese Datei aufrufen
-require './roman_number_generator'
-
-puts "Insert a number between 1 and 3999"
-num = gets
-puts RomanNumberGenerator.new.convert_number_to_roman(num.to_i)
--- Uebung 3 - Rails/rails_tutorial_app_chris/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RailsTutorialAppChris::Application.config.secret_token = 'f2fe1bd24d35336556eb66e437a75cdb0ec36518a76b44154675a65d7e1cef5102d113e022ac38ecf310e62beb076edd6304621a1bcd08e65f6de8a1c06f09d5'
