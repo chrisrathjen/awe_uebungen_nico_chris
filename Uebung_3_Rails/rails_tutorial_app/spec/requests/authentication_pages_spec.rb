90412d5c2b37f60d55a41c08da8d49c545be9d86
Christian Rathjen <chrisr@tzi.de>
2012-02-23 02:43:06 +0100

    sign-in and out works...

--- Uebung_3_Rails/rails_tutorial_app/spec/requests/authentication_pages_spec.rb
+++ Uebung_3_Rails/rails_tutorial_app/spec/requests/authentication_pages_spec.rb
@@ -19,6 +19,11 @@ describe "Authentication" do
 
         it { should have_selector('title', text: 'Sign in') }
         it { should have_selector('div.flash.error', text: 'Invalid') }
+        
+        describe "after visiting another page" do
+          before { click_link "Home" }
+          it { should_not have_selector('div.flash.error') }
+        end
       end
       
       describe "with valid information" do
@@ -34,5 +39,6 @@ describe "Authentication" do
             it { should have_link('Sign out', href: signout_path) }
             it { should_not have_link('Sign in', href: signin_path) }
           end
+          
     end
 end
