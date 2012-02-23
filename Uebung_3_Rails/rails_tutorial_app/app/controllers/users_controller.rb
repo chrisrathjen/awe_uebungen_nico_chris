b3fd6c688f5ed5bacb1155c8bca83f99c05a5639
Christian Rathjen <chrisr@tzi.de>
2012-02-23 16:00:02 +0100

    friendly redirect

--- Uebung_3_Rails/rails_tutorial_app/app/controllers/users_controller.rb
+++ Uebung_3_Rails/rails_tutorial_app/app/controllers/users_controller.rb
@@ -1,27 +1,30 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+
   def new
     @user = User.new
   end
-  
+
   def show
-      @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
-  
+
   def create
-      @user = User.new(params[:user])
-      if @user.save
-        sign_in @user
-        flash[:success] = "Welcome to the Sample App!"
-        redirect_to @user
-      else
-        render 'new'
-      end
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
@@ -31,5 +34,19 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  private
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in." #notice is flash[:notice]
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 end
